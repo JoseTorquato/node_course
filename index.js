b91689d248dd8259f1bb1b58b7d438c25c68d619
José Lucas <jltorquato12@gmail.com>
2020-12-20 23:33:16 -0300

    Criando rotas com parametros fixo, query e opcionais

--- index.js
+++ index.js
@@ -6,12 +6,38 @@ app.get("/", function(req, res){
     res.send("Bem Vindo!");
 });
 
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao meu blog!")
+app.get("/blog/:artigo?", function(req, res){// Parâmentro OPCIONAL 
+
+    let artigo = req.params.artigo;
+
+    // Condição para saber se pssou ou não parâmetro
+    if(artigo){
+        res.send(`Artigo: ${artigo}.`);
+    }else{
+        res.send("Bem vindo ao meu blog!");
+    };
+    
 });
 
-app.get("/canal/youtube", function(req, res){
+app.get("/canal/youtube", function(req, res){// QUERY params
+
+    let canal = req.query["canal"];
+
+    if(canal){
+        res.send(canal)
+    }else{
+        res.send("Nenhum canal fornecido!")
+    };
+
     res.send("Bem vindo ao meu canal!")
+});
+
+app.get("/ola/:nome/:empresa", function(req, res){// Parametro obrigatório
+    // REQ => DADOS ENVIADOS PELO USUÁRIO
+    // RES => RESPOSTA QUE VAI ER ENVIADA PARA O USUÁRIO
+    let nome = req.params.nome
+    let empresa = req.params.empresa
+    res.send(`<h1> Oi, ${nome}! Você trabalha na empresa: ${empresa}...`);
 });
 
 
